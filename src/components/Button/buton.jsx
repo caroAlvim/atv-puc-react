52d7d4e37f81551f7cc4d4c9432e27de3c3bb907
Carolina Alvim <caro.alvim@gmail.com>
2022-03-18 18:17:31 -0300

    feat: login funcionando

---
+++ src/components/Button/buton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Button({
+  buttonOnClick,
+  buttonClass,
+  children,
+
+}) {
+  return (
+
+    <button onClick={buttonOnClick} className={buttonClass}>{children}</button>
+  );
+}
+
+export default Button;
