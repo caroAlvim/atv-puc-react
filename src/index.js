08bb5140078bde17b1f367a3dcbd1e501158bc2e
Carolina Alvim <caro.alvim@gmail.com>
2022-04-13 12:59:39 -0300

    ajustes para deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Rotas from './Routes';
+import Rotas from './routes';
 import reportWebVitals from './reportWebVitals';
 import './firebase';
 
